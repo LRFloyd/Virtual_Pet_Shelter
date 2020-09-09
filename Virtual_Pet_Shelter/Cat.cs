0193658b61f31dc678a25e8af0710c1cdc3abada
LRFloyd <lavelle.floyd@yahoo.com>
2020-09-09 14:05:51 -0400

    add constuctor to cat class

--- Virtual_Pet_Shelter/Cat.cs
+++ Virtual_Pet_Shelter/Cat.cs
@@ -4,7 +4,32 @@ using System.Text;
 
 namespace Virtual_Pet_Shelter
 {
-    class Cat
+    class Cat : VirtualPet
     {
+        //fields and properties are inherent from parent(VirtualPet) class
+        //properties specific to this class
+        public string Type { get; set; }
+        public string Diet { get; set; }
+
+        //constructors
+        public Cat()
+        {
+            //default constructor
+        }
+
+        //preset constructors to set type 
+        public Cat(string name)
+        {
+            this.Name = name;
+            this.Diet = "vegitarian";
+            this.Type = "Persian";
+        }
+
+        public Cat(string name, string description, int age) : base(false,false,false)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Description = description;
+        }
     }
 }
